fc8400f9c65125323486c5cf9ea845853e432e32
santanukumar-learner <122003878+santanukumar-learner@users.noreply.github.com>
2023-08-25 20:33:07 +0530

    Coffee_machine

---
+++ test.py
@@ -0,0 +1,28 @@
+class CoffeeMaker:
+    """Models the machine that makes the coffee"""
+    def __init__(self):
+        self.resources = {
+            "water": 300,
+            "milk": 200,
+            "coffee": 100,
+        }
+
+    def report(self):
+        """Prints a report of all resources."""
+        print(f"Water: {self.resources['water']}ml")
+        print(f"Milk: {self.resources['milk']}ml")
+        print(f"Coffee: {self.resources['coffee']}g")
+
+    def is_resource_sufficient(self, drink):
+        """Returns True when order can be made, False if ingredients are insufficient."""
+        can_make = True
+        for item in drink.ingredients:
+            print(item)
+            if drink.ingredients[item] > self.resources[item]:
+                print(f"Sorry there is not enough {item}.")
+                can_make = False
+        return can_make
+from menu import Menu
+menu = Menu()
+drink = menu.find_drink(latte)
+CoffeeMaker.is_resource_sufficient(drink=drink)
